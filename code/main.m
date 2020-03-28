c0bca4ae5f4cbd0de67dad2186faa9d13a58bd83
guillerui <62159813+guillerui@users.noreply.github.com>
2020-03-28 12:43:56 +0100

    Update main.m

--- code/main.m
+++ code/main.m
@@ -15,6 +15,15 @@ left and middle checkerboard images, and another calibration with the same proce
 load('calibration/stereoParamLtM');
 load('calibration/stereoParamMtR');
 
+% Images acquisition
+I_Left = imread("images/subject1/subject1Left/subject1_Left_1.jpg");
+I_Middle = imread("images/subject1/subject1Middle/subject1_Middle_1.jpg");
+I_Right = imread("images/subject1/subject1Right/subject1_Right_1.jpg");
+
+% Rectification of the images
+[I_Left_Recti, I_LeftMid_Recti] = rectifyStereoImages(I_Left, I_Middle, stereoParamLtM);
+[I_MidRight_Recti, I_Right_Recti] = rectifyStereoImages(I_Middle, I_Right, stereoParamMtR);
+
 % Colour normalization script
 I1 = imread("images/subject1/subject1Middle/subject1_Middle_1.jpg");
 I2 = imread("images/subject1/subject1Middle/subject1_Middle_364.jpg");
