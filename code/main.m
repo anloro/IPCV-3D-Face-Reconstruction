45f4886f5f867198e82b01422b7e49baf6998ddb
guillerui <62159813+guillerui@users.noreply.github.com>
2020-03-28 11:39:23 +0100

    Update main.m

--- code/main.m
+++ code/main.m
@@ -1,6 +1,20 @@
 clear
 close all
 
+% Camera calibration
+%{
+Estimation of the geometric parameters of the stereo camera. It's necessary to select the same amount
+of images from both perspectives in order to allow the estimation to compare them. Once the automatic
+selection of points from the checkerboards had been done, it will appear the stereo pairs processed and the 
+calibration will take place. The elimination of some pairs will help to reduce the reprojection errors from
+both cameras. The session will be saved and paraments exported. This calibration will take place within the 
+left and middle checkerboard images, and another calibration with the same process between middle and right.
+%}
+
+% stereoCameraCalibrator
+load('stereoParamLtM');
+load('stereoParamMtR');
+
 % Colour normalization script
 I1 = imread("images/subject1/subject1Middle/subject1_Middle_1.jpg");
 I2 = imread("images/subject1/subject1Middle/subject1_Middle_364.jpg");
