1697f71c347c45f3b620712749302b919c4bd5f7
lhomme_a <a.r.y.lhomme@student.utwente.nl>
2020-04-04 23:33:33 +0200

    disparityMap

--- code/disparityMap.m
+++ code/disparityMap.m
@@ -1,23 +1,24 @@
 function [disparityMap] = disparityMap(imLeft,imRight,disparityRange)
 %DISPARITYMAP Create the disparity map of 2 images
 
-figure
-imshow(imLeft);
-figure
-imshow(imRight);
-
 imLeftGray = rgb2gray(imLeft);
 imRightGray = rgb2gray(imRight);
 
-disparityMap = disparityBM(imLeftGray,imRightGray,...
+disparityMap = disparity(imLeftGray,imRightGray,...
     'DisparityRange', disparityRange, ...
     'UniquenessThreshold', 1, ...
-    'ContrastThreshold', 0.5, ...
-    'DistanceThreshold',5, ...
+    'ContrastThreshold', 0.7, ...
+    'DistanceThreshold',15, ...
     'BlockSize', 5);
+
+%disparityMap = disparitySGM(imLeftGray,imRightGray,...
+%    'DisparityRange', disparityRange, ...
+%    'UniquenessThreshold', 100);
+
 disparityMap(imcomplement(imLeftGray > 0)) = 0;
+
 % Clear noise
-disparityMap = medfilt2(disparityMap, [80 80],'symmetric');
+disparityMap = medfilt2(disparityMap, [50 50], 'symmetric');
 
 % Remove holes 
 disparityMap = imfill(disparityMap, 'holes');
